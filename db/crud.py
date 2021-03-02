b70f491ca6ae3d2a13edd0d318f5a676a7e8e588
GabrielGramlich <mb4510qj@go.minneapolis.edu>
2021-03-01 18:13:37 -0600

    Add crud class with pass functions

---
+++ db/crud.py
@@ -0,0 +1,23 @@
+crud.py
+
+def setup_database():
+	#TODO create user table
+	#TODO create credential table
+	#TODO create trip table
+	pass
+
+
+def create_row():
+	pass
+
+
+def read_row():
+	pass
+
+
+def update_row():
+	pass
+
+
+def delete_row():
+	pass
