1e1d0a11156c42c5ce5114c1c87e96dd8711e891
GabrielGramlich <mb4510qj@go.minneapolis.edu>
2021-03-03 16:02:41 -0600

    Hash passwords with a salt

--- db/authenticate.py
+++ db/authenticate.py
@@ -1,9 +1,17 @@
+import hashlib
+
+
 def create_salt():
 	pass
 
 
-def hash_password():
-	pass
+def hash_password(password, salt):
+	salted_password = f'{password}{salt}'	# Add password and salt for added security
+	byte_string = str.encode(salted_password)	# Convert to bytes to for hashing
+	salty_hashbrowns = hashlib.sha512(byte_string)	# Hash bytes using SHA
+	hashed_in_int = hash(salty_hashbrowns)	# Convert to int for easy comparison/storage
+
+	return hashed_in_int
 
 
 def passwords_match():
