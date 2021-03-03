e7cf9147db0e05ebb13dab04309584346d82ed0b
GabrielGramlich <mb4510qj@go.minneapolis.edu>
2021-03-03 16:09:23 -0600

    Move salt before password

--- db/authenticate.py
+++ db/authenticate.py
@@ -3,7 +3,7 @@ import crypt
 
 
 def hash_password(password, salt):
-	salted_password = f'{password}{salt}'	# Add password and salt for added security
+	salted_password = f'{salt}{password}'	# Add password and salt for added security
 	byte_string = str.encode(salted_password)	# Convert to bytes to for hashing
 	salty_hashbrowns = hashlib.sha512(byte_string)	# Hash bytes using SHA
 	hashed_in_int = hash(salty_hashbrowns)	# Convert to int for easy comparison/storage
