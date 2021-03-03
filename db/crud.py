1a5e1c5640ebdb01c8cba97db16a362a90d6ce71
GabrielGramlich <mb4510qj@go.minneapolis.edu>
2021-03-03 15:08:07 -0600

    Add NOT NULL constraints with comments

--- db/crud.py
+++ db/crud.py
@@ -5,19 +5,24 @@ def setup_database():
 	
 	'''Username column is used for signing in, and providing foreign key to cred and trip
 	 tables. Name column is used for printing meaningful messages to the user.'''
-	create_sql = 'CREATE TABLE IF NOT EXISTS users (user_id INTEGER PRIMARY KEY, username TEXT UNIQUE, name TEXT)'
+	create_sql = 'CREATE TABLE IF NOT EXISTS users (user_id INTEGER PRIMARY KEY, username TEXT UNIQUE NOT NULL, name TEXT)'
 	create_table(create_sql)
 	'''Username acts as foreign key from users table; email address acts as alternate form of
 	 authentication on sign in; password is stored as SHA-256 hashed value of password; salt is
 	 stored to apply to password before hashing to entered password to verify stored encrypted
 	 password and entered passwords match.'''
-	create_sql = 'CREATE TABLE IF NOT EXISTS credentials (credential_id INTEGER PRIMARY KEY, username TEXT UNIQUE, email_address TEXT UNIQUE, password TEXT UNIQUE, salt TEXT UNIQUE, FOREIGN KEY(username) REFERENCES users(username))'
+	'''NOT NULL contraint not listed under email_address for the case the user only wished to
+	 provide one method of verification.'''
+	create_sql = 'CREATE TABLE IF NOT EXISTS credentials (credential_id INTEGER PRIMARY KEY, username TEXT UNIQUE NOT NULL, email_address TEXT UNIQUE, password TEXT UNIQUE NOT NULL, salt TEXT UNIQUE NOT NULL, FOREIGN KEY(username) REFERENCES users(username))'
 	create_table(create_sql)
 	'''Username acts as a foreign key from users table; trip_start_time is time user called, or
 	 planned from their ride; start/end lat/long columns are the location start and end points;
 	 price is estimate gained from uber, or calculated value based on distance from transit;
 	 is_transit is a boolean value determining if ride share or public transit.'''
-	create_sql = 'CREATE TABLE IF NOT EXISTS trips (trip_id INTEGER PRIMARY KEY, username TEXT, trip_start_time DATETIME, trip_start_lat DECIMAL, trip_start_long DECIMAL, trip_stop_lat DECIMAL, trip_stop_long DECIMAL, price DECIMAL, is_transit BOOLEAN, FOREIGN KEY(username) REFERENCES users(username))'
+	'''NOT NULL constraints not listed under start time and location, price, or is_transit for
+	 the case the user is saving a favorite route, not interesting result, e.g. going to a close
+	 friends.'''
+	create_sql = 'CREATE TABLE IF NOT EXISTS trips (trip_id INTEGER PRIMARY KEY, username TEXT NOT NULL, trip_start_time DATETIME, trip_start_lat DECIMAL, trip_start_long DECIMAL, trip_stop_lat DECIMAL NOT NULL, trip_stop_long DECIMAL NOT NULL, price DECIMAL, is_transit BOOLEAN, FOREIGN KEY(username) REFERENCES users(username))'
 	create_table(create_sql)
 
 	'''Either this, or the previous create_sql strings and create_table calls should be commented
@@ -28,7 +33,10 @@ def setup_database():
 	 planned from their ride; start/end address columns are the location start and end points;
 	 price is estimate gained from uber, or calculated value based on distance from transit;
 	 is_transit is a boolean value determining if ride share or public transit.'''
-	create_sql = 'CREATE TABLE IF NOT EXISTS trips (trip_id INTEGER PRIMARY KEY, username TEXT, trip_start_time DATETIME, trip_start_address TEXT, trip_start_city TEXT, trip_start_state TEXT, trip_stop_address TEXT, trip_stop_city TEXT, trip_stop_state TEXT, price DECIMAL, is_transit BOOLEAN, FOREIGN KEY(username) REFERENCES users(username))'
+	'''NOT NULL constraints not listed under start time and location, price, or is_transit for
+	 the case the user is saving a favorite route, not interesting result, e.g. going to a close
+	 friends.'''
+	create_sql = 'CREATE TABLE IF NOT EXISTS trips (trip_id INTEGER PRIMARY KEY, username TEXT NOT NULL, trip_start_time DATETIME, trip_start_address TEXT, trip_start_city TEXT, trip_start_state TEXT, trip_stop_address TEXT NOT NULL, trip_stop_city TEXT NOT NULL, trip_stop_state TEXT NOT NULL, price DECIMAL, is_transit BOOLEAN, FOREIGN KEY(username) REFERENCES users(username))'
 	create_table(create_sql)
 
 
