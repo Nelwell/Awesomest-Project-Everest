4c3facf6077d14e3680321b88ec302b5ac8627cc
Nelwell <nick.elwell206@gmail.com>
2021-03-10 02:12:31 -0600

    add ability to take in a US address through the use of a Location object and convert it to lat/long through the convert_to_lat_long Location class method

--- api/location.py
+++ api/location.py
@@ -1,10 +1,38 @@
-def convert_to_lat_long():
-	pass
+from geopy import Nominatim
+
+"""
+Remove lines 8-11 and 38 before approving/merging to main branch, as well as this docstring comment.
+"""
 
 
-def convert_to_address():
-	pass
+def main():
+    address = Location('Minneapolis', 'MN', '1501 Hennepin Ave')  # example location object, MCTC address
+    lat, long = Location.convert_to_lat_long(address)  # pass examples to method to convert to lat/long
+    print(f'Test Latitude = {lat}, Test Longitude = {long}')  # for testing
 
 
-def convert_to_city():
-	pass
+class Location:
+
+    def __init__(self, city, state, st_address=''):
+        self.city = city
+        self.state = state
+        self.st_address = st_address
+
+    @staticmethod
+    def convert_to_lat_long(address):
+        # adapted from https://towardsdatascience.com/geocode-with-python-161ec1e62b89
+        locator = Nominatim(user_agent='carless-app')
+        location = locator.geocode(f'{address.st_address} '
+                                   f'{address.city} '
+                                   f'{address.state}')
+        lat, long = location.latitude, location.longitude
+        return lat, long
+
+    def convert_to_address(self):
+        pass
+
+    def convert_to_city(self):
+        pass
+
+
+main()
