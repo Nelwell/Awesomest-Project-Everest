441b4a6dbc01da846c9170c65ef49f41b5b89988
Nelwell <nick.elwell206@gmail.com>
2021-03-13 16:42:46 -0600

    make email field case-insensitive, reformat create table statements for better readability, and add .sqlite file to .gitignore

--- db/crud.py
+++ db/crud.py
@@ -2,6 +2,7 @@ import sqlite3
 
 DATABASE = 'awesomest_db.sqlite'
 
+
 def setup_database():
 	'''The three tables are users: storing base information about users; credentials: storing
 	 information to authenticate within the application; and trips: storing api data about
@@ -11,14 +12,19 @@ def setup_database():
 	 tables. Name column is used for printing meaningful messages to the user.'''
 	create_sql = 'CREATE TABLE IF NOT EXISTS users (user_id INTEGER PRIMARY KEY, username TEXT UNIQUE NOT NULL, name TEXT)'
 	create_table(create_sql)
+
 	'''Username acts as foreign key from users table; email address acts as alternate form of
 	 authentication on sign in; password is stored as SHA-256 hashed value of password; salt is
 	 stored to apply to password before hashing to entered password to verify stored encrypted
 	 password and entered passwords match.'''
-	'''NOT NULL contraint not listed under email_address for the case the user only wished to
+	'''NOT NULL constraint not listed under email_address for the case the user only wished to
 	 provide one method of verification.'''
-	create_sql = 'CREATE TABLE IF NOT EXISTS credentials (credential_id INTEGER PRIMARY KEY, username TEXT UNIQUE NOT NULL, email_address TEXT UNIQUE, password TEXT UNIQUE NOT NULL, salt TEXT UNIQUE NOT NULL, FOREIGN KEY(username) REFERENCES users(username))'
+	create_sql = 'CREATE TABLE IF NOT EXISTS credentials (credential_id INTEGER PRIMARY KEY, ' \
+				 'username TEXT UNIQUE NOT NULL, email_address TEXT UNIQUE COLLATE NOCASE, ' \
+				 'password TEXT UNIQUE NOT NULL, salt TEXT UNIQUE NOT NULL, ' \
+				 'FOREIGN KEY(username) REFERENCES users(username))'
 	create_table(create_sql)
+
 	'''Username acts as a foreign key from users table; trip_start_time is time user called, or
 	 planned from their ride; start/end lat/long columns are the location start and end points;
 	 price is estimate gained from uber, or calculated value based on distance from transit;
@@ -26,7 +32,10 @@ def setup_database():
 	'''NOT NULL constraints not listed under start time and location, price, or is_transit for
 	 the case the user is saving a favorite route, not interesting result, e.g. going to a close
 	 friends.'''
-	create_sql = 'CREATE TABLE IF NOT EXISTS trips (trip_id INTEGER PRIMARY KEY, username TEXT NOT NULL, trip_start_time DATETIME, trip_start_lat DECIMAL, trip_start_long DECIMAL, trip_stop_lat DECIMAL NOT NULL, trip_stop_long DECIMAL NOT NULL, price DECIMAL, is_transit BOOLEAN, FOREIGN KEY(username) REFERENCES users(username))'
+	create_sql = 'CREATE TABLE IF NOT EXISTS trips (trip_id INTEGER PRIMARY KEY, username TEXT NOT NULL, ' \
+				 'trip_start_time DATETIME, trip_start_lat DECIMAL, trip_start_long DECIMAL, ' \
+				 'trip_stop_lat DECIMAL NOT NULL, trip_stop_long DECIMAL NOT NULL, price DECIMAL, is_transit BOOLEAN, ' \
+				 'FOREIGN KEY(username) REFERENCES users(username))'
 	create_table(create_sql)
 
 	'''Either this, or the previous create_sql strings and create_table calls should be commented
@@ -40,7 +49,10 @@ def setup_database():
 	'''NOT NULL constraints not listed under start time and location, price, or is_transit for
 	 the case the user is saving a favorite route, not interesting result, e.g. going to a close
 	 friends.'''
-	create_sql = 'CREATE TABLE IF NOT EXISTS trips (trip_id INTEGER PRIMARY KEY, username TEXT NOT NULL, trip_start_time DATETIME, trip_start_address TEXT, trip_start_city TEXT, trip_start_state TEXT, trip_stop_address TEXT NOT NULL, trip_stop_city TEXT NOT NULL, trip_stop_state TEXT NOT NULL, price DECIMAL, is_transit BOOLEAN, FOREIGN KEY(username) REFERENCES users(username))'
+	create_sql = 'CREATE TABLE IF NOT EXISTS trips (trip_id INTEGER PRIMARY KEY, username TEXT NOT NULL, ' \
+				 'trip_start_time DATETIME, trip_start_address TEXT, trip_start_city TEXT, trip_start_state TEXT, ' \
+				 'trip_stop_address TEXT NOT NULL, trip_stop_city TEXT NOT NULL, trip_stop_state TEXT NOT NULL, ' \
+				 'price DECIMAL, is_transit BOOLEAN, FOREIGN KEY(username) REFERENCES users(username))'
 	create_table(create_sql)
 
 	'''Trip_id acts as a foreign key from users table; temperature is temperature X amount of
@@ -49,7 +61,8 @@ def setup_database():
 	'''NOT NULL constraints not listed under temperature and description for the case the api
 	 returns None for that location and time. Constraint should be placed if check is made
 	 elsewhere to prevent this.'''
-	create_sql = 'CREATE TABLE IF NOT EXISTS weather_details (weather_id INTEGER PRIMARY KEY, trip_id INTEGER NOT NULL, temperature_f DECIMAL, description TEXT, FOREIGN KEY(trip_id) REFERENCES trips(trip_id))'
+	create_sql = 'CREATE TABLE IF NOT EXISTS weather_details (weather_id INTEGER PRIMARY KEY, trip_id INTEGER NOT NULL, ' \
+				 'temperature_f DECIMAL, description TEXT, FOREIGN KEY(trip_id) REFERENCES trips(trip_id))'
 	create_table(create_sql)
 
 
