fc5b48a62431d83a4112991e823c7cfc24c1e3b3
Orion <gn9081ca@go.minneapolis.edu>
2021-03-16 21:40:06 -0500

    Fixed the testing environment in vscode and added more tests for emails.

--- tests/test_validate.py
+++ tests/test_validate.py
@@ -4,6 +4,29 @@ from validation.validate import *
 class ValidatorTest(unittest.TestCase):
 
     def test_is_valid_email(self):
-        test_email = "hi@hello.com"
+
+        #correct format should return true
+        test_email = "hi@hello.com" 
         response = is_valid_email(test_email)
         self.assertTrue(response)
+
+        #bad format should return false
+        test_email = "hi@hello"
+        response = is_valid_email(test_email)
+        self.assertFalse(response)
+        
+        #checking Top level domains with two letter separators
+        test_email = "hi@hello.co.uk"
+        response = is_valid_email(test_email)
+        self.assertTrue(response) 
+
+        #checking spaces
+        test_email = "hi @hello.co.uk"
+        response = is_valid_email(test_email)
+        self.assertFalse(response) 
+
+        #checking incorrect symbols
+        test_email = "hi*@hello.co.uk"
+        response = is_valid_email(test_email)
+        self.assertFalse(response)
+    
--- validation/test_validate.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from validation.validate import *
-
-class ValidatorTest(unittest.TestCase):
-
-    def test_is_valid_email(self):
-        test_email = "hi@hello.com"
-        response = is_valid_email(test_email)
-        self.assertTrue(response)
