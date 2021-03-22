10ca37800563fcf90c4869ece8f4151915ae3688
Nelwell <nick.elwell206@gmail.com>
2021-03-22 06:42:07 -0500

    add documentation to location.py, remove test code and create unittest in test_location.py module

--- api/location.py
+++ api/location.py
@@ -1,14 +1,9 @@
 from geopy import Nominatim
 
 """
-Remove lines 8-11 and 38 before approving/merging to main branch, as well as this docstring comment.
+Module to create Location class.
+Holds city, state, St address and converts to lat/lon for API calls
 """
-
-
-def main():
-    address = Location('Minneapolis', 'MN', '1501 Hennepin Ave')  # example location object, MCTC address
-    lat, long = Location.convert_to_lat_long(address)  # pass examples to method to convert to lat/long
-    print(f'Test Latitude = {lat}, Test Longitude = {long}')  # for testing
 
 
 class Location:
@@ -19,20 +14,17 @@ class Location:
         self.st_address = st_address
 
     @staticmethod
-    def convert_to_lat_long(address):
+    def convert_to_lat_lon(address):
         # adapted from https://towardsdatascience.com/geocode-with-python-161ec1e62b89
         locator = Nominatim(user_agent='carless-app')
         location = locator.geocode(f'{address.st_address} '
                                    f'{address.city} '
                                    f'{address.state}')
-        lat, long = location.latitude, location.longitude
-        return lat, long
+        lat, lon = location.latitude, location.longitude
+        return lat, lon
 
     def convert_to_address(self):
         pass
 
     def convert_to_city(self):
         pass
-
-
-main()
