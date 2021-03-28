f3bdcc2a0dde48ad630450d6f26fe68e57510586
Nelwell <nick.elwell206@gmail.com>
2021-03-28 15:37:34 -0500

    some starter menu code

--- main.py
+++ main.py
@@ -1,6 +1,41 @@
+from ui import display
+from ui.objects import Menu
+# from artwork import Artwork
+
+# artwork_store = ArtworkStore()
+
+
 def main():
-	pass
+    menu = create_menu()
+
+    while True:
+        choice = display.display_menu(menu)
+        action = menu.get_action(choice)
+        action()
+        if choice == 'Q':
+            break
+
+
+def create_menu():
+    menu = Menu()
+    menu.add_option('1', 'Add Artist', add_artist)
+    # menu.add_option('2', 'See All Artist\'s Artwork', show_all_artist_artwork)
+    # menu.add_option('3', 'See Artist\'s Available Artwork', show_artist_available_artwork)
+    # menu.add_option('4', 'Add Artist\'s Artwork', add_artwork)
+    # menu.add_option('5', 'Delete Artwork', delete_artwork)
+    # menu.add_option('6', 'Change Artwork Availability Status', change_availability)
+    # menu.add_option('Q', 'Quit', quit_program)
+
+    return menu
+
+
+def add_artist():
+    new_artist = display.get_location_info()
+    try:
+        new_artist.insert_artist()
+    except:
+        print('This artist is already in the database.')
 
 
 if __name__ == '__main__':
-	main()
+    main()
--- ui/display.py
+++ ui/display.py
@@ -1,6 +1,29 @@
-def display_menu():
-	pass
+def display_menu(menu):
+    """ Displays all of the menu options, checks that the user enters a valid choice and returns the choice.
+     :param menu: the menu to display
+     :returns: the user's choice """
+    while True:
+        print(menu)
+        choice = input('Enter choice? ').upper()  # makes the quit option case insensitive
+        if menu.is_valid(choice):
+            return choice
+        else:
+            print('Not a valid choice, try again.')
+
+
+def message(msg):
+    """ Prints a message for the user
+     :param msg: the message to print"""
+    print(msg)
+
+
+def get_location_info():
+    """ Create new Location object from name and email provided by user
+     :returns: an Artist created from the name and email. """
+    name = input('Enter artist\'s name: ')
+    email = input('Enter artist\'s email: ')
+    return Artist(name, email)
 
 
 def display_data():
-	pass
+    pass
