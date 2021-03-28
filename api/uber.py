67cae658a54131c734afa5ba302e90d7a888b404
GabrielGramlich <mb4510qj@go.minneapolis.edu>
2021-03-28 01:24:16 -0500

    Initialize primary variables with init

--- api/uber.py
+++ api/uber.py
@@ -8,5 +8,9 @@ class UberTrip():
 	duration = None
 	distance = None
 
-	def __init__(self):
-		pass
+	def __init__(self, start_lat, start_lon, end_lat, end_lon, trip_time=time.strftime("%H:%M:%S",time.localtime())):
+		self.start_lat = start_lat
+		self.start_lon = start_lon
+		self.end_lat = end_lat
+		self.end_lon = end_lon
+		self.trip_time = trip_time
