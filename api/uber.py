26cd084ea9f481a011e622a4a68bab51cd884334
GabrielGramlich <mb4510qj@go.minneapolis.edu>
2021-03-28 01:22:45 -0500

    Add class variables

--- api/uber.py
+++ api/uber.py
@@ -1,3 +1,12 @@
 class UberTrip():
+	start_lat = None
+	start_lon = None
+	end_lat = None
+	end_lon = None
+	trip_time = None
+	price = None
+	duration = None
+	distance = None
+
 	def __init__(self):
 		pass
