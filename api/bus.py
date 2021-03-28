32e3eae056bd4310e737a316e364ebdcd7027706
Orion <gn9081ca@go.minneapolis.edu>
2021-03-28 00:49:21 -0500

    Pulling Json Data from API

--- api/bus.py
+++ api/bus.py
@@ -3,14 +3,13 @@ import os
 import requests
 import logging
 from requests.models import Response
-#from validation.validate import is_valid_json
-from .validation.validate import *
+#from ..validation.validate import *
 
 key = os.environ.get("bing_maps_key")
 
-def foo(bar):
-    if (len(bar) > 1):
-        print(bar)
+# def foo(bar):
+#     if (len(bar) > 1):
+#         print(bar)
 
 class BusTrip:
     start_location = None
@@ -24,15 +23,13 @@ class BusTrip:
     
     units = 'imperial'
 
-    def __init__(self, start_location, end_location, start_time):
+    def __init__(self, start_location, end_location, start_time): #Format: "Golden Gate Bridge", "Fisherman's Wharf", '05:42:00'
         if start_location != None and end_location != None and start_time !=None:
             self.start_location = start_location
             self.end_location = end_location
             self.start_time = start_time
             self.refresh_trip()
         
-
-
     def __str__(self) -> str:
         return f'Start: {self.start_location} End: {self.end_location} Time: {self.time} Cost: {self.cost}'
 
@@ -51,41 +48,27 @@ class BusTrip:
             self.refresh_trip()
 
     def refresh_trip(self):
-        #TODO Make API all
-        #TODO get json
-        #TODO yup trip_json - requests.get()
         #TODO extract json Data
-        #try:
-            
-        query = {'travelMode': self.mode, 'waypoint1': self.start_location, 'waypoint2':self.end_location, 'dateTime':self.start_time, 'appid': key}
-        #url = f'http://dev.virtualearth.net/REST/v1/Routes/{travelMode}?wayPoint.1={wayPoint1}&viaWaypoint.2={viaWaypoint2}&waypoint.3={waypoint3}&wayPoint.n={waypointN}&heading={heading}&optimize={optimize}&avoid={avoid}&distanceBeforeFirstTurn={distanceBeforeFirstTurn}&routeAttributes={routeAttributes}&timeType={timeType}&dateTime={dateTime}&maxSolutions={maxSolutions}&tolerances={tolerances}&distanceUnit={distanceUnit}&key={BingMapsKey}'
-        #url = '%http://dev.virtualearth.net/REST/V1/Routes/Transit?wp.0=Golden%20Gate%20Bridge&wp.1=Fishermans%20Wharf&timeType=Departure&dateTime=3:00:00PM&output=xml&key={key}'
-        url = 'http://dev.virtualearth.net/REST/v1/Routes/'
-        response = requests.get(url, params=query)
-        logging.debug(f'Request sent:\n{response}')
-        response.raise_for_status()
-        data = response.json()
-        if is_valid_json(data):
+        try:
+            print(self.start_location, self.end_location, self.start_time)
+            query = {'travelMode': self.mode, 'waypoint.1': self.start_location, 'waypoint.2':self.end_location, 'dateTime':self.start_time, 'key': key}
+            url = 'http://dev.virtualearth.net/REST/v1/Routes/'
+            print(requests.get(url, params=query))
+            response = requests.get(url, params=query)
+            print(f'Request sent:\n{response}')
+            response.raise_for_status()
+            data = response.json()
             logging.info(f'Data recieved:\n{data}')
-            return data, None
+            print(data)
+            # if is_valid_json(data):
+            #     logging.info(f'Data recieved:\n{data}')
+            #     return data, None
 
 
-        #except Exception as e:
-        #    logging.exception(e)
-        #    logging.exception(response.text)
-        #    return None, e
-
-query = {'key': key}
-#url = 'http://dev.virtualearth.net/REST/v1/Routes/{travelMode}?wayPoint.1={wayPoint1}&viaWaypoint.2={viaWaypoint2}&waypoint.3={waypoint3}&wayPoint.n={waypointN}&heading={heading}&optimize={optimize}&avoid={avoid}&distanceBeforeFirstTurn={distanceBeforeFirstTurn}&routeAttributes={routeAttributes}&timeType={timeType}&dateTime={dateTime}&maxSolutions={maxSolutions}&tolerances={tolerances}&distanceUnit={distanceUnit}&key={BingMapsKey}'
-url = f'%http://dev.virtualearth.net/REST/V1/Routes/Transit?wp.0=Golden%20Gate%20Bridge&wp.1=Fishermans%20Wharf&timeType=Departure&dateTime=3:00:00PM&output=xml&key={key}'
-url = 'http://dev.virtualearth.net/REST/v1/Routes'
-response = requests.get(url, params=query)
-print(type(response))
-logging.debug(f'Request sent:\n{response}')
-response.raise_for_status()
-data = response.json()
-logging.info(f'Data recieved:\n{data}')
+        except Exception as e:
+           logging.exception(e)
+           logging.exception(response.text)
+           return None, e
 
 # print(data)
-var = BusTrip("Golden Gate Bridge", "Fisherman's Wharf", dt.now())
-print(var)
+var = BusTrip("Golden Gate Bridge", "Fisherman's Wharf", '05:42:00')
